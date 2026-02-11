287d9885265107b620023e7835e9a624a87781f6
aakitibharathraj <aakitibunny63@gmail.com>
2026-02-11 01:31:13 -0800

    Add files via upload

---
+++ style.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int you,date;
+    printf("enter the rating of you and date \n");
+    scanf("%d %d",&you,&date);
+    if(you<=2||date<=2)
+    {
+        printf("0");
+    }
+    else if (you>=8||date>=8)
+    {
+        printf("2");
+    }
+    else
+    {
+        printf("1");
+    }
+    return 0;
+}
+output:
+enter the rating of you and date 
+2
+5
+0
